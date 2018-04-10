555c3e4f7c7ce41152ec3dedf67321a256aa85dc
Steve Valasek <svalasek@gmail.com>
2018-04-09 20:39:18 -0700

    Monday night, search issues

--- keys.js
+++ keys.js
@@ -1,14 +1,14 @@
 console.log("this is loaded");
 
-exports.twitter = {
-    consumer_key: process.env.twitterConsKey,
-    consumer_sectre: process.env.twitterConsSecret,
-    access_token_key: process.env.twitterAccTokenKey,
-    access_token_secret: process.env.twitterAccTokenSecret,
+exports.Twitter = {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_sectre: process.env.WTITTER_CONSUMER_SECRET,
+    access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 };
 
-exports.spotify = {
-    id: process.env.spotifyId,
-    secret: process.env.spotifySecret,
+exports.Spotify = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
 
 };
--- liri.js
+++ liri.js
@@ -3,24 +3,33 @@ require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require("fs");
 var request = require("request");
-var spotify = new Spotify(keys.spotify);
+var Twitter = require("twitter");
+var spotify = require("spotify");
+var Spotify = require("node-spotify-api");
+fs.writeFile("log.txt", process.argv, (error) => { /* handle error */ });
+var spotify = new Spotify(keys.Spotify);
 var client = new Twitter(keys.twitter);
+var nodeArgs = "";
 
-var command = process.argv[2];
+var action = process.argv[2];
 var nodeArgs = process.argv;
 
 var value = "";
 
-for (var i = 3; i < nodeArgs.length; i++){
+for (var i = 3; i < nodeArgs.length; i++) {
+
     if (i > 3 && i < nodeArgs.length){
         
         value = value + "+" + nodeArgs[i];
-    }
+
+        console.log(nodeArgs);
+        console.log("argv: " + process.argv);
+    };
 };
 
 //commands
 
-switch (action){
+switch (action) {
     case "my-tweets":
     getTweets();
     break;
@@ -40,7 +49,10 @@ switch (action){
 
 //"my-tweets" returns last 20 tweets
 function getTweets() {
-    client.get("statuses/user_timeline", steve626, 20, function(error, tweets, response){
+    var params = {screen_name: "steve626",
+                    count: 20};
+
+    client.get("statuses/user_timeline", params, function(error, tweets, response) {
         //shows tweets if there are no errors and if there is a response
         if (!error && response.statusCode === 200) {         
             console.log("last " + tweets.length + " tweets: " + tweets);
@@ -53,8 +65,9 @@ function getTweets() {
                 console.log("trouble retrieving tweets, try again later");
                 fs.appendFile("log.txt", "trouble retrieving tweets" + Date());
         
-    };
-}
+        };
+    }
+)};
 
 //"spotify-this-song"
 
@@ -62,11 +75,11 @@ function getSong(value) {
     if (value == null) {
         value = "v9SXd0LRQxOaczjL3hbiOQ";
     }
-    request("https://api.spotify.com/v  /search?q=" + value + "&type=track", function(error, response, body){
-        if (!error && response.statusCode == 200) {
-            spotBody = JSON.stringify(body);
+    spotify.search({type: "track", query: value }, function(err, data, response) {
+        if (!err ) {
+            spotBody = JSON.stringify(data);
             fs.appendFile("log.txt", spotBody + Date());
-            console.log(body)
+            console.log(data)
             console.log("\n~~~~~~~~~~~~~~~~~~~~~~\n");
             console.log("Artist: " + spotBody.name);
             console.log("Song: "  );//add song location
@@ -74,24 +87,25 @@ function getSong(value) {
             console.log("Link: " ); //add link location
             console.log("\n~~~~~~~~~~~~~~~~~~~~~~\n");
 
-        }  if (error) {
-            console.log("error retrieving song: \n" + error);
-            fs.appendFile("log.txt", error + Date());
+        }  if (err) {
+            console.log("error retrieving song: " + err);
+            fs.appendFile("log.txt", err + Date());
         } else {
             console.log("trouble retrieving songs, try again later");
             fs.appendFile("log.txt", "trouble retrieving songs" + Date());
         }
     });
 
-});
+};
 
 //"movie-this"
 function getMovie(value) {
     if (value == null) {
         value = "shakes the clown";
+        console.log(value);
     }
-    request("http://www.omdbapi.com/?t=" + value + "&tomatoes=true&y=&plot=short&apikey=4585ce0c");
-                if (!error && response.statusCode == 200) {
+    request("http://www.omdbapi.com/?t=" + value + "&tomatoes=true&y=&plot=short&apikey=4585ce0c", function(err, body){
+                if (!err ) {
                     movieBody = JSON.stringify(body);
                     fs.appendFile("log.txt", movieBody + Date());
                     console.log("\n~~~~~~~~~~~~~~~~~~~~~~\n");
@@ -104,20 +118,24 @@ function getMovie(value) {
                     console.log("Plot: " + movieBody.Plot);
                     console.log("Cast: " + movieBody.Actors);
                     console.log("\n~~~~~~~~~~~~~~~~~~~~~~\n");
-                }                    
+                
+                console.log("body: " + body);
+                
+                }
 
-         if (error) {
-            console.log("error retrieving movie: \n" + error);
-            fs.appendFile("log.txt", error + Date());
+         if (err) {
+            console.log("error retrieving movie: \n" + err);
+            fs.appendFile("log.txt", err + Date());
         } else {
             console.log("trouble retrieving movies, try again later");
             fs.appendFile("log.txt", "trouble retrieving movie" + Date());
-    }
+        }
+    });
 };
 
 //"do-what-it-says"
 
-function random() {
+function getRandom() {
     fs.readFile("random.txt", "utf8", function(error,data){
         if (error) {
             console.log(error);
